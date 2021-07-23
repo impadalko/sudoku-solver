673912bb497840645023a3175899038a014eebb3
Ivan Padalko <dev@impadalko.com>
2021-07-23 09:42:44 -0300

    Solve given board
    This only works if the board is valid

--- src/index.js
+++ src/index.js
@@ -24,4 +24,43 @@ solveButton.addEventListener('click', () => {
       cell.removeAttribute('contenteditable')
     }
   }
+  solveButton.setAttribute('disabled', true)
+  // TODO: Check if valid before attempting to solve
+  // TODO: Display to the user if a solution was found
+  solve(grid)
 })
+
+const solve = async (grid) => {
+  for (let i = 0; i < 9; i++)
+    for (let j = 0; j < 9; j++)
+      if (grid[i][j] === 0) {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(grid, i, j, k)) {
+            const cell = document.getElementById(`cell-${i}-${j}`)
+            // Attempt to solve by setting cell[i][j] to k and recursing
+            grid[i][j] = k
+            cell.innerHTML = `${k}<br>`
+            // Add sleep to better show the backtracking to the user
+            await new Promise((r) => setTimeout(r, 50))
+            if (await solve(grid)) return true
+            // Rollback if cannot solve with this value
+            grid[i][j] = 0
+            cell.innerHTML = '<br>'
+          }
+        }
+        return false
+      }
+  return true
+}
+
+const isValid = (grid, row, col, candidate) => {
+  for (let i = 0; i < 9; i++) if (grid[i][col] !== 0 && grid[i][col] === candidate) return false
+  for (let i = 0; i < 9; i++) if (grid[row][i] !== 0 && grid[row][i] === candidate) return false
+  const squareRow = Math.floor(row / 3)
+  const squareCol = Math.floor(col / 3)
+  for (let i = 0; i < 9; i++) {
+    const cell = grid[3 * squareRow + Math.floor(i / 3)][3 * squareCol + (i % 3)]
+    if (cell !== 0 && cell === candidate) return false
+  }
+  return true
+}
