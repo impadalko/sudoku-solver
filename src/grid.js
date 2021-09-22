987beaf200738ae025f96727280bf72e25e57abe
Ivan Padalko <dev@impadalko.com>
2021-09-22 20:33:40 -0300

    Move Grid to class

--- src/grid.js
+++ src/grid.js
@@ -1,38 +1,76 @@
-const solveButton = document.getElementById('solve-button')
+export class Grid {
+  constructor(gridSize) {
+    this.gridSize = gridSize
+    this.squareSize = Math.sqrt(gridSize)
+    this.data = Array.from(Array(gridSize), () => new Array(gridSize).fill(0))
+    this.cells = Array.from(Array(gridSize), () => new Array(gridSize))
 
-const valueValidator = (gridSize) => {
-  const regex = gridSize === 9 ? /^[1-9]?<br>$/ : /^([1-9]|1[0-6])?<br>$/
-  return (e) => {
-    if (!e.currentTarget.innerHTML.match(regex)) e.currentTarget.classList.add('invalid')
-    else e.currentTarget.classList.remove('invalid')
+    const solveButton = document.getElementById('solve-button')
+    solveButton.removeAttribute('disabled')
+    const table = document.getElementById('sudoku-grid')
+    table.innerHTML = ''
 
-    if (document.querySelector('.invalid')) solveButton.setAttribute('disabled', true)
-    else solveButton.removeAttribute('disabled')
-  }
-}
+    const regex = this.gridSize === 9 ? /^[1-9]?<br>$/ : /^([1-9]|1[0-6])?<br>$/
+    for (let i = 0; i < this.gridSize; i++) {
+      const row = table.insertRow()
+      for (let j = 0; j < this.gridSize; j++) {
+        const cell = row.insertCell()
+        this.cells[i][j] = cell
+        cell.setAttribute('id', `cell-${i}-${j}`)
+        cell.setAttribute('contenteditable', '')
+        if (i === 0) cell.classList.add('border-top')
+        if (j === 0) cell.classList.add('border-left')
+        if (i % this.squareSize === this.squareSize - 1) cell.classList.add('border-bottom')
+        if (j % this.squareSize === this.squareSize - 1) cell.classList.add('border-right')
+        cell.innerHTML = '<br>'
+        cell.addEventListener('input', (e) => {
+          if (!e.currentTarget.innerHTML.match(regex)) e.currentTarget.classList.add('invalid')
+          else {
+            e.currentTarget.classList.remove('invalid')
+            this.data[i][j] = Number.parseInt(e.currentTarget.innerText.replace('\n', '')) || 0
+          }
 
-// TODO: move this to a class
-export const createGrid = (gridSize) => {
-  const squareSize = Math.sqrt(gridSize)
-  const grid = Array.from(Array(gridSize), () => new Array(gridSize).fill(0))
-  solveButton.removeAttribute('disabled')
-  const table = document.getElementById('sudoku-grid')
-  table.innerHTML = ''
-
-  const inputValidator = valueValidator(gridSize)
-  for (let i = 0; i < gridSize; i++) {
-    const row = table.insertRow()
-    for (let j = 0; j < gridSize; j++) {
-      const cell = row.insertCell()
-      cell.setAttribute('id', `cell-${i}-${j}`)
-      cell.setAttribute('contenteditable', '')
-      if (i === 0) cell.classList.add('border-top')
-      if (j === 0) cell.classList.add('border-left')
-      if (i % squareSize === squareSize - 1) cell.classList.add('border-bottom')
-      if (j % squareSize === squareSize - 1) cell.classList.add('border-right')
-      cell.innerHTML = '<br>'
-      cell.addEventListener('input', inputValidator)
+          if (document.querySelector('.invalid')) solveButton.setAttribute('disabled', true)
+          else solveButton.removeAttribute('disabled')
+        })
+      }
     }
   }
-  return grid
+
+  lock() {
+    for (let i = 0; i < this.gridSize; i++)
+      for (let j = 0; j < this.gridSize; j++) {
+        const cell = this.cells[i][j]
+        cell.innerHTML = `<b>${cell.innerHTML}</b>`
+        cell.removeAttribute('contenteditable')
+      }
+  }
+
+  isValid() {
+    for (let i = 0; i < this.gridSize; i++)
+      for (let j = 0; j < this.gridSize; j++)
+        if (this.data[i][j] !== 0 && !this.isValidCell(i, j, this.data[i][j])) return false
+    return true
+  }
+
+  isValidCell(row, col, candidate) {
+    for (let i = 0; i < this.gridSize; i++)
+      if (i !== row && this.data[i][col] === candidate) return false
+    for (let i = 0; i < this.gridSize; i++)
+      if (i !== col && this.data[row][i] === candidate) return false
+
+    const squareRow = Math.floor(row / this.squareSize)
+    const squareCol = Math.floor(col / this.squareSize)
+    for (let i = 0; i < this.gridSize; i++) {
+      const l = this.squareSize * squareRow + Math.floor(i / this.squareSize)
+      const k = this.squareSize * squareCol + (i % this.squareSize)
+      if (!(l === row && k === col) && this.data[l][k] === candidate) return false
+    }
+    return true
+  }
+
+  updateCell(row, col, value) {
+    this.data[row][col] = value
+    this.cells[row][col].innerHTML = `${value ? value : ''}<br>`
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
-import { createGrid } from './grid.js'
-import { isValidCell, isValidGrid } from './valid.js'
+import { Grid } from './grid.js'
 
-let gridSize = 9
-let grid = createGrid(gridSize)
+let grid = new Grid(9)
 
 for (const el of document.querySelectorAll('input[type=radio][name=size]')) {
   el.addEventListener('change', (e) => {
-    gridSize = parseInt(e.currentTarget.value, 10)
+    const gridSize = parseInt(e.currentTarget.value, 10)
     document.querySelector(':root').style.setProperty('--grid-size', `${432 / gridSize}px`)
-    grid = createGrid(gridSize)
+    grid = new Grid(gridSize)
   })
 }
 
-const solveButton = document.getElementById('solve-button')
 // TODO: Add algorithm selector and implement dancing links
-solveButton.addEventListener('click', () => {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      const cell = document.getElementById(`cell-${i}-${j}`)
-      grid[i][j] = Number.parseInt(cell.innerText.replace('\n', '')) || 0
-      cell.innerHTML = `<b>${cell.innerHTML}</b>`
-      cell.removeAttribute('contenteditable')
-    }
-  }
-  solveButton.setAttribute('disabled', true)
+const solveButton = document.getElementById('solve-button')
+solveButton.addEventListener('click', (e) => {
+  grid.lock()
+  e.currentTarget.setAttribute('disabled', true)
   const status = document.getElementById(`status`)
-  if (!isValidGrid(grid)) {
+  if (!grid.isValid()) {
     status.innerHTML = 'Cannot solve'
     return
   }
@@ -37,21 +28,18 @@ solveButton.addEventListener('click', () => {
 })
 
 const solve = async (grid) => {
-  for (let i = 0; i < gridSize; i++)
-    for (let j = 0; j < gridSize; j++)
-      if (grid[i][j] === 0) {
-        for (let k = 1; k <= gridSize; k++) {
-          if (isValidCell(grid, i, j, k)) {
-            const cell = document.getElementById(`cell-${i}-${j}`)
+  for (let i = 0; i < grid.gridSize; i++)
+    for (let j = 0; j < grid.gridSize; j++)
+      if (grid.data[i][j] === 0) {
+        for (let k = 1; k <= grid.gridSize; k++) {
+          if (grid.isValidCell(i, j, k)) {
             // Attempt to solve by setting cell[i][j] to k and recursing
-            grid[i][j] = k
-            cell.innerHTML = `${k}<br>`
+            grid.updateCell(i, j, k)
             // Add sleep to better show the backtracking to the user
             await new Promise((r) => setTimeout(r, 50))
             if (await solve(grid)) return true
             // Rollback if cannot solve with this value
-            grid[i][j] = 0
-            cell.innerHTML = '<br>'
+            grid.updateCell(i, j, 0)
           }
         }
         return false
